dc2b6baf35e5deb8881c959ec8c2df08d872464a
Kurisuka <kadilerk@gmail.com>
2018-01-03 19:09:23 +0000

    hello?

---
+++ Lil BloodPump/Lil BloodPump/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Lil_BloodPump
+
+{
+    using Aimtec;
+    using Aimtec.SDK.Events;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameEvents.GameStart += GameEvents_GameStart;
+            //Gapcloser.OnGapcloser += Gapclose;
+        }
+
+        private static void GameEvents_GameStart()
+        {
+            if (ObjectManager.GetLocalPlayer().ChampionName != "Vladimir")
+                return;
+
+            var Vladimir = new Vladimir();
+        }
+    }
+}
